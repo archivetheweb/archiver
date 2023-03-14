62389cf8c146a2e91112d821cb966a13e2681195
Dev43 <patrick@bitbackapp.com>
2023-03-14 11:40:41 +0200

    fixes to crawler

--- archiver/src/crawler.rs
+++ archiver/src/crawler.rs
@@ -59,42 +59,42 @@ impl Crawler {
         &mut self,
         should_terminate: Arc<AtomicBool>,
     ) -> anyhow::Result<CrawlResult> {
-        // we setup a channel for new url
-        // this channel will send an (String, Vec<String>,i32) tuple
-        // first element being the url visited, next element being all the new urls and last being the depth of the visited_url
+        // we setup a channel for the a new URL. This channel will send an (String, Vec<String>,i32) tuple,
+        // the first element being the url visited, next element being all the new links found on the page,
+        // and last being the depth of the visited_url
         let (scraped_urls_tx, mut scraped_urls_rx) =
             mpsc::channel::<(String, Vec<String>, i32)>(self.concurrent_tabs as usize + 10);
 
         let (visit_url_tx, visit_url_rx) = mpsc::channel::<(String, i32)>(1000);
         let (failed_url_tx, mut failed_url_rx) = mpsc::channel::<(String, i32)>(1000);
 
-        let active_browsers = Arc::new(AtomicUsize::new(0));
+        let active_tabs = Arc::new(AtomicUsize::new(0));
 
         self.processor(
             scraped_urls_tx.clone(),
             visit_url_rx,
             failed_url_tx,
-            active_browsers.clone(),
+            active_tabs.clone(),
         );
 
         // we send the first url to crawl
         visit_url_tx.send((self.url.clone(), 0)).await.unwrap();
 
         while !should_terminate.load(Ordering::Relaxed) {
-            // we take in new urls
+            // we receive the scraped urls
             let res = scraped_urls_rx.try_recv();
 
             if res.is_ok() {
                 let (visited_url, new_scraped_urls, depth) = res.unwrap();
                 debug!(
-                    "Adding {} as a visited url at depth {}",
+                    "adding {} as a visited url at depth {}",
                     &visited_url, depth
                 );
                 self.visited.insert(visited_url.to_string());
                 let new_urls: HashSet<String> = HashSet::from_iter(new_scraped_urls);
                 for new_url in new_urls.iter() {
                     if !self.visited.contains(new_url) && depth < self.depth {
-                        debug!("Adding {} to the queue", &new_url);
+                        debug!("adding {} to the queue", &new_url);
                         match visit_url_tx.send((new_url.to_string(), depth + 1)).await {
                             Ok(_) => {}
                             Err(e) => {
@@ -102,12 +102,6 @@ impl Crawler {
                             }
                         };
                     }
-                }
-            } else {
-                // we check if channel is empty
-                match res.err().unwrap() {
-                    mpsc::error::TryRecvError::Empty => {}
-                    mpsc::error::TryRecvError::Disconnected => debug!("disconnected"),
                 }
             }
 
@@ -117,7 +111,7 @@ impl Crawler {
                         match self.failed.get_mut(&url.to_string()) {
                             Some(count) if count <= &mut self.url_retries => {
                                 warn!(
-                                    "Retrying url {} at d={}, retried {} so far",
+                                    "retrying url {} at d={}, retried {} so far",
                                     url, depth, count
                                 );
                                 // we resend the url to be fetched
@@ -133,7 +127,7 @@ impl Crawler {
                                 *count = *count + 1;
                             }
                             None => {
-                                warn!("Retrying url {} at d={}, retried {} so far", url, depth, 0);
+                                warn!("first retry of url {} at d={}", url, depth);
                                 self.failed.insert(url.to_string(), 0);
                                 // this could be blocking if not in it's own thread or not enough buffer
                                 match visit_url_tx.send((url.clone(), depth)).await {
@@ -152,9 +146,10 @@ impl Crawler {
                 }
             }
 
+            // if all of our channels are at capacity and we have no active tabs, we are done
             if scraped_urls_tx.capacity() == scraped_urls_tx.max_capacity()
                 && visit_url_tx.capacity() == visit_url_tx.max_capacity()
-                && active_browsers.load(Ordering::SeqCst) == 0
+                && active_tabs.load(Ordering::SeqCst) == 0
             {
                 break;
             }
@@ -193,50 +188,36 @@ impl Crawler {
         });
     }
 
-    fn fetch_pdf(url: String) -> anyhow::Result<()> {
-        match reqwest::blocking::get(url.as_str()) {
-            Ok(res) => {
-                debug!("fetching pdf at {}", url.as_str());
-                let _r = res.text();
-                return Ok(());
-            }
-            Err(e) => {
-                warn!("error downloading pdf err: {}", e);
-                return Err(anyhow::anyhow!(e));
-            }
-        }
-    }
-
     fn processor(
         &self,
         scraped_urls_tx: mpsc::Sender<(String, Vec<String>, i32)>,
         visit_url_rx: mpsc::Receiver<(String, i32)>,
         failed_url_tx: mpsc::Sender<(String, i32)>,
-        active_browsers: Arc<AtomicUsize>,
+        active_tabs: Arc<AtomicUsize>,
     ) {
-        let concurrency = self.concurrent_tabs;
+        let concurrent_tabs = self.concurrent_tabs;
         let base_url = self.base_url.clone();
-        let start_url = self.url.clone();
-        let m = self.main_title.clone();
+        let original_url = self.url.clone();
+        let title = self.main_title.clone();
         let min_wait = self.min_wait_secs;
         let max_wait = self.max_wait_secs;
         let timeout = self.timeout;
         tokio::spawn(async move {
             tokio_stream::wrappers::ReceiverStream::new(visit_url_rx)
-                .for_each_concurrent(concurrency as usize, |queued_url| {
+                .for_each_concurrent(concurrent_tabs as usize, |queued_url| {
                     let (url, depth) = queued_url.clone();
-                    debug!("browsing {} at depth {}", url, depth);
+                    debug!("crawling {} at depth {}", url, depth);
 
-                    let ab = active_browsers.clone();
-                    let tx = scraped_urls_tx.clone();
+                    let at = active_tabs.clone();
+                    let scraped_urls_tx = scraped_urls_tx.clone();
                     let failed_url_tx = failed_url_tx.clone();
                     let u = url.clone();
                     let base_url = base_url.clone();
-                    let is_first_url = start_url == u;
-                    let title_mutex = m.clone();
+                    let is_first_url = original_url == u;
+                    let title_mutex = title.clone();
 
                     async move {
-                        ab.fetch_add(1, Ordering::SeqCst);
+                        at.fetch_add(1, Ordering::SeqCst);
 
                         let links = task::spawn_blocking(move || {
                             // headless chrome can't handle pdfs, so we make a direct request for it
@@ -262,7 +243,7 @@ impl Crawler {
                                 if head.is_err() {
                                     warn!(
                                         "error browsing for {} with tab err {}, head err {}",
-                                        u,
+                                        &u,
                                         tab.err().unwrap(),
                                         head.err().unwrap()
                                     );
@@ -286,7 +267,7 @@ impl Crawler {
                                     } else {
                                         warn!(
                                             "error browsing for {} with tab err {}",
-                                            u,
+                                            &u,
                                             tab.err().unwrap(),
                                         );
                                         // we return an empty list of links, and flag as errored out
@@ -296,25 +277,25 @@ impl Crawler {
                             }
                             let tab = tab.unwrap();
                             if is_first_url {
-                                let title = match tab.get_title() {
-                                    Ok(t) => t,
+                                match tab.get_title() {
+                                    Ok(title) => {
+                                        let mut main_title = title_mutex.blocking_lock();
+                                        *main_title = title;
+                                    }
                                     Err(e) => {
                                         warn!("could not get title {:?}", e);
-                                        "".into()
                                     }
                                 };
-                                let mut main_title = title_mutex.blocking_lock();
-                                *main_title = title;
                             }
 
-                            (
+                            return (
                                 browser
                                     .get_links(&tab)
                                     .iter()
                                     .filter_map(normalize_url_map(base_url.into()))
                                     .collect::<Vec<String>>(),
                                 false,
-                            )
+                            );
                         })
                         .await;
 
@@ -322,7 +303,7 @@ impl Crawler {
                             Ok(l) => l,
                             Err(e) => {
                                 error!("problem spawning a blocking thread {}", e);
-                                ab.fetch_sub(1, Ordering::SeqCst);
+                                at.fetch_sub(1, Ordering::SeqCst);
                                 failed_url_tx.send((url, depth)).await.unwrap();
                                 return;
                             }
@@ -339,14 +320,14 @@ impl Crawler {
                                 }
                             };
                         } else {
-                            match tx.send((url, links.0, depth)).await {
+                            match scraped_urls_tx.send((url, links.0, depth)).await {
                                 Ok(_) => {}
                                 Err(e) => {
-                                    error!("could not send to tx {}", e)
+                                    error!("could not send to scraped_urls_tx {}", e)
                                 }
                             };
                         }
-                        ab.fetch_sub(1, Ordering::SeqCst);
+                        at.fetch_sub(1, Ordering::SeqCst);
                     }
                 })
                 .await;
@@ -354,7 +335,22 @@ impl Crawler {
             return;
         });
     }
+
     pub fn url(&self) -> String {
         self.url.to_string()
+    }
+
+    fn fetch_pdf(url: String) -> anyhow::Result<()> {
+        match reqwest::blocking::get(url.as_str()) {
+            Ok(res) => {
+                debug!("fetching pdf at {}", url.as_str());
+                let _r = res.text();
+                return Ok(());
+            }
+            Err(e) => {
+                warn!("error downloading pdf err: {}", e);
+                return Err(anyhow::anyhow!(e));
+            }
+        }
     }
 }
